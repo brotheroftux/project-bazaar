d031014571fa0183aa6727a87df4c79e19593e8a
Daniel Kuznetsov <afc2008@yandex.ru>
2018-03-02 00:46:18 +0300

    Added icon component

---
+++ frontend/src/components/misc/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg :viewBox='viewBox' class='svg-host'>
+        <use :href='id' />
+    </svg>
+</template>
+
+<script>
+export default {
+    props: ['icon'],
+    computed: {
+        viewBox: function () {
+            return this.icon.viewBox
+        },
+        id: function () {
+            return `#${this.icon.id}`
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+    @import '~@/global-styles/vars.sass'
+    .svg-host
+        display: block
+        width: $icon-size
+        height: $icon-size
+        fill: $icon-fill
+</style>
