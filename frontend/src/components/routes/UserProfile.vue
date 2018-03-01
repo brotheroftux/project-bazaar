484fe8e464fc16f1f25a198316199b47a7e59876
Unknown <VeryCrazySlava@gmail.com>
2018-03-01 17:27:30 +0300

    WIP

--- frontend/src/components/routes/UserProfile.vue
+++ frontend/src/components/routes/UserProfile.vue
@@ -1,16 +1,57 @@
 <template>
     <div class="user-profile">
+        <div class="info">
+            <div class="header">
+                <div class="user-photo">
+                    <!-- photo here-->
+                    <img style="height:100%" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Windows_live_square.JPG">
+                </div>
+                <div class="text-info">
+                    <div class="fullName">
+                         {{user.lastname}} {{user.firstname}} {{user.patronomic}}
+                    </div>
+                    <div class="education">
+                        <!-- education icon here-->
+                        <div>
+                            {{user.education.qualification}}, {{user.education.degree}}, {{user.education.university}}
+                        </div>
+                    </div>
+                    <div class="work">
+                        <!-- work icon here-->
+                        <div>
+                            {{user.work.position}}, {{user.work.company}}
+                        </div>
+                    </div>
+                    <div class="email">
+                        {{user.email}}
+                    </div>
+                    <!-- need to add buttonClick Handler-->
+                    <ui-button text="Связаться" class="connect"></ui-button>
+                </div>
+            </div>
+            <div class="tabs">
+                
+            </div>
+        </div>
+        <div class="skills">
 
+        </div>
     </div>
 </template>
 
 <script>
+/*
 import store from '@/store'
 import userProfile from '@/store/modules/userProfile'
 
-import { mapActions } from 'vuex'
-
+import { mapState, mapActions } from 'vuex'
+*/
+import UiButton from '@/components/core-ui/UIButton.vue'
 export default {
+    /*
+    computed: mapState('userProfile', [
+        'user'
+    ]),
     methods: mapActions('userProfile', [
         'getUserProfile'
     ]),
@@ -30,10 +71,58 @@ export default {
     beforeRouteLeave: function (to, from, next) {
         store.unregisterModule('userProfile')
         next()
+    },
+*/
+    data: function () { 
+        return { 
+            user: {
+                lastname:'Панасовец',
+                firstname:'Вячеслав',
+                patronomic:'Викторович',
+                education : {
+                    qualification: 'Нубас'
+                },
+                work : {
+
+                }
+            }
+        }
+    },
+
+    components: {
+        UiButton
     }
+    
 }
 </script>
 
 <style lang="scss" scoped>
 
+    .user-profile {
+        display:flex;
+    }
+
+    .user-photo {
+        height:100%;
+    }
+
+    .info {
+        display:flex;
+    }
+
+    .skills {
+        display:flex;
+    }
+
+    .text-info {
+        display:flex;
+        flex-direction:column;
+        justify-content: space-around;
+    }
+    
+    .header {
+        display:flex;
+
+        max-height:100px;
+    }
 </style>
