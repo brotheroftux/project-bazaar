a86739735e8afb440ccd5e7c27cc27c430210c72
Daniel Kuznetsov <afc2008@yandex.ru>
2018-03-01 13:56:33 +0300

    Auth implementation

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,6 @@
-import api from '@/util/api/urls.js'
+import postJSON from '@/util/postJSON'
+
+import api from '@/api/urls.js'
 
 export async function getUserProfile (id) {
     const data = await fetch(api.getUserProfile(id))
@@ -9,4 +11,15 @@ export async function getUserProfile (id) {
         return parsed.response
     else
         return undefined
+}
+
+export async function requestMagic (email) {
+    const result = await postJSON(api.requestMagic, { email })
+
+    const parsed = result.json()
+
+    if (parsed.hasOwnProperty('response'))
+        return true
+    else
+        return false
 }
