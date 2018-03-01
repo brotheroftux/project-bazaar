828c9e1b9b362f5be0513b8f5ddffe59fcbe7ae9
Unknown <VeryCrazySlava@gmail.com>
2018-03-01 13:19:02 +0300

    UIButton

---
+++ frontend/src/components/UI-.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :class="className" @click = "$emit('buttonClick')"></button>
+</template>
+
+<script>
+export default {
+    props: ['className']
+}
+</script>
+
+<style lang='scss'>
+    @import '~@/global-styles/vars.sass';
+    
+    .authButton {
+        color:blue;
+    }
+
+</style>
