ffd94a6365af4efa6cb06298b909585b3fc94ce4
Daniel Kuznetsov <afc2008@yandex.ru>
2018-03-01 11:02:42 +0300

    Added vue-router to the boilerplate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from '@/components/App.vue'
 
 import store from '@/store'
+import router from '@/router'
 
 import '@/global-styles/global.sass'
 
 new Vue({
     el: '#app-root',
     store,
+    router,
     render: h => h(App)
 })
