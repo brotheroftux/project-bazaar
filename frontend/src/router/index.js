06003ba6ab8f77e65f909e7acc9bb534c734767d
Daniel Kuznetsov <afc2008@yandex.ru>
2018-03-01 13:58:07 +0300

    Initial branch commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// route components
+import UserProfile from '@/components/routes/UserProfile.vue'
+
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+    { path: '/user/:id/', component: UserProfile }
+]
 
 export default new VueRouter({
     mode: 'history',
