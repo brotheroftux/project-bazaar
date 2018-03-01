b86d76b2c73bc6a581fe788f607063ebb65e46e3
Unknown <VeryCrazySlava@gmail.com>
2018-03-01 15:31:23 +0300

    Alert fix

---
+++ frontend/src/components/core-ui/Alert.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class = 'alert'>
+        {{alertContent}}
+    </div>
+</template>
+
+<script>
+
+ 
+export default {
+    props: ['alertContent'],
+}
+</script>
+
+<style lang='scss'>
+    @import '~@/global-styles/vars.sass';
+
+    .alert {
+        display: block;
+        border-radius: 5px;
+        position: absolute;
+        padding: 10px;
+        width: 200px;
+        font-size: 0.8rem;
+        background-color: rgb(197, 197, 197);
+        border-style: solid;
+        border-width: 1px;
+        word-wrap: break-word;
+    }
+
+</style>
